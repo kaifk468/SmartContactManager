40c8f6154c47910d6f78a7cb6cf99c26973e7528
747974 <kaifk468@gmail.com>
2022-08-06 01:44:24 +0530

    stated creating user dashboard

--- src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserControler.java
+++ src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserControler.java
@@ -1,16 +1,35 @@
 package com.smartcontactmanager.smartcontactmanager.Controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smartcontactmanager.smartcontactmanager.Dao.UserRepo;
+import com.smartcontactmanager.smartcontactmanager.Entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserControler {
 
+    @Autowired
+    UserRepo userRepo;
+
     @RequestMapping("/index")
-    public String userIndex()
+    public String userIndex(Model model,Principal principal)
     {
+
+        String userName=principal.getName();
+        User user=userRepo.getUserByUserName(userName);
+
+        model.addAttribute("user", user);
         return "user/index";
+
+
     }
+
+    //public String 
     
 }
